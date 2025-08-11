17a99d225e23c85b5045142f366b6b0470720df5
rukkiecodes <rukkiecodes@gmail.com>
2025-08-11 04:00:08 +0100

    lunch

--- routes/recido/generate.js
+++ routes/recido/generate.js
@@ -28,11 +28,8 @@ router.post("/generate-pdf", async (req, res) => {
       headless: true,
       args: [
         "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu",
-      ],
-      executablePath: process.env.CHROME_PATH, // set by the new buildpack
+        "--disable-setuid-sandbox"
+      ]
     });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
